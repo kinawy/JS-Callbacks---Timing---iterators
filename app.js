cd5bb0cac4c7425e130de52216a603cbd988c914
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-16 18:07:01 -0400

    Callback greetings

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+function greeting (name, callback) {
+    let sentence = 'Hello, my name is ' + name;
+    // return sentence;
+    let otherSentence = callback();
+
+    return sentence + otherSentence;
+}
+
+let myName = 'Sameh';
+console.log(greeting(myName, anotherGreeting));
+
+function anotherGreeting () {
+    let sentence = ' and I forgot my last name.'
+    return sentence;
+}
